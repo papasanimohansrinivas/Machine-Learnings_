53fb69a6c7522cd4196090196fa233822e9f6890
papasani\mohan/srinivas <papasani.mohansrinivas@gmail.com>
2017-10-06 19:09:36 +0530

    Delete accuracy.py

--- KNN/accuracy.py
+++
@@ -1,9 +0,0 @@
-def accuracy(result,validation):
-    
-    r1 = len(result)
-    
-    matches = lambda v1,v2,r1: [1 if v1[r]==v2[r] else 0 for r in xrange(r1)]
-
-    print matches(result,validation,r1)    
-    
-    return float(matches(result,validation,r1).count(1))/len(result)*100
